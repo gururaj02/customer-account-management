ca7ea05e878b2190287fb1058609e696877cb760
gururaj02 <ngururaj58@gmail.com>
2025-02-09 00:02:49 +0530

    Add project files.

---
+++ customer-account-management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace customer_account_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
